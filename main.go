80749f072b309ad39546f01438ea52461ecdece5
Chao Xu <xuchao@google.com>
2017-01-03 00:19:46 -0800

    second draft

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"flag"
-
-	"github.com/golang/glog"
+	"fmt"
+	"os"
+	"time"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/runtime"
+	"k8s.io/client-go/pkg/util/wait"
 	"k8s.io/client-go/pkg/util/workqueue"
 	"k8s.io/client-go/pkg/watch"
 	"k8s.io/client-go/tools/cache"
@@ -35,29 +38,114 @@ func getClientsetOrDie(kubeconfig string) *kuberntes.Clientset {
 func main() {
 	kubeconfig := flag.String("kubeconfig", "", "Path to a kube config. Only required if out-of-cluster.")
 	flag.Parse()
-	controller := newServiceLookupController(kubeconfig)
+	controller := newendpointLookupController(kubeconfig)
 }
 
-type ServiceLookupManager struct {
+type serviceLookupController struct {
 	clientset *kubernetes.Clientset
-	// A cache of services
-	serviceStore cache.StoreToServiceLister
-	// Watches changes to all services
-	serviceController *cache.Controller
+	// A cache of endpoints
+	endpointsStore cache.StoreToendpointLister
+	// Watches changes to all endpoints
+	endpointController *cache.Controller
 	// A cache of pods
 	podStore cache.StoreToPodLister
 	// Watches changes to all pods
 	podController cache.Controller
 
-	podsQueue     workqueue.RateLimitingInterface
-	servicesQueue workqueue.RateLimitingInterface
+	addressToPod   addressToPod
+	podsQueue      workqueue.RateLimitingInterface
+	endpointsQueue workqueue.RateLimitingInterface
 }
 
-func newServiceLookupController(kubeconfig) *ServiceLookupManager {
-	slm := &ServiceLookupManager{
-		kubeClient:    getClientsetOrDie(kubeconfig),
-		podsQueue:     workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "pods"),
-		servicesQueue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "services"),
+func (slm *serviceLookupController) enqueuePod(obj interface{}) {
+	key, err := controller.KeyFunc(obj)
+	if err != nil {
+		fmt.Printf("Couldn't get key for object %+v: %v", obj, err)
+		return
+	}
+	slm.podsQueue.Add(key)
+}
+
+func (slm *serviceLookupController) podWorker() {
+	workFunc := func() bool {
+		key, quit := slm.podsQueue.Get()
+		if quit {
+			return true
+		}
+		defer slm.podsQueue.Done(key)
+
+		obj, exists, err := slm.podStore.Indexer.GetByKey(key)
+		if !exists {
+			fmt.Printf("Pod has been deleted %v", key)
+			return false
+		}
+
+		pod := obj.(*v1.Pod)
+		slm.addressToPod.Write(pod.Status.PodIP, pod)
+		return false
+	}
+	for {
+		if quit := workFunc(); quit {
+			fmt.Printf("pod worker shutting down")
+			return
+		}
+	}
+}
+
+func (slm *serviceLookupController) enqueueendpoint(obj interface{}) {
+	key, err := controller.KeyFunc(obj)
+	if err != nil {
+		fmt.Printf("Couldn't get key for object %+v: %v", obj, err)
+		return
+	}
+	slm.endpointsQueue.Add(key)
+}
+
+func (slm *serviceLookupController) endpointWorker() {
+	workFunc := func() bool {
+		key, quit := slm.endpointsQueue.Get()
+		if quit {
+			return true
+		}
+		defer slm.endpointsQueue.Done(key)
+
+		obj, exists, err := slm.endpointsStore.Indexer.GetByKey(key)
+		if !exists {
+			fmt.Printf("endpoint has been deleted %v", key)
+			return false
+		}
+
+		endpoints := obj.(*v1.endpoints)
+
+		for _, subset := range endpoints.Subsets {
+			for _, address := range subset.EndpointAddress {
+				// TODO: need a lock here
+				pod, ok := slm.addressToPod.Read(address)
+				if !ok {
+					slm.endpointsQueue.AddRateLimited(key)
+					return false
+				}
+				output := bytes.NewBuffer()
+				fmt.FPrintf(output, "Pod [%s:%s] is backing service %s", pod.ObjectMeta.Name, address, endpoints.ObjectMeta.Name)
+			}
+		}
+
+		fmt.FPrintf(os.Stderr, output.String())
+		return false
+	}
+	for {
+		if quit := workFunc(); quit {
+			fmt.Printf("pod worker shutting down")
+			return
+		}
+	}
+}
+
+func newendpointLookupController(kubeconfig) *serviceLookupController {
+	slm := &serviceLookupController{
+		kubeClient:     getClientsetOrDie(kubeconfig),
+		podsQueue:      workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "pods"),
+		endpointsQueue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "endpoints"),
 	}
 
 	slm.podStore.Indexer, slm.podController = cache.NewIndexerInformer(
@@ -65,7 +153,7 @@ func newServiceLookupController(kubeconfig) *ServiceLookupManager {
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 				return slm.kubeClient.Core().Pods(api.NamespaceAll).List(options)
 			},
-			WatchFunc: func(options api.ListOptions) (watch.Interface, error) {
+			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				return slm.kubeClient.Core().Pods(api.NamespaceAll).Watch(options)
 			},
 		},
@@ -77,39 +165,43 @@ func newServiceLookupController(kubeconfig) *ServiceLookupManager {
 		},
 	)
 
-	slm.serviceStore.Indexer, slm.serviceController = cache.NewIndexerInformer(
+	slm.endpointsStore.Indexer, slm.endpointController = cache.NewIndexerInformer(
 		&cache.ListWatch{
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
-				return slm.kubeClient.Core().Services(api.NamespaceAll).List(options)
+				return slm.kubeClient.Core().Endpoints(api.NamespaceAll).List(options)
 			},
 			WatchFunc: func(options api.ListOptions) (watch.Interface, error) {
-				return slm.kubeClient.Core().Services(api.NamespaceAll).Watch(options)
+				return slm.kubeClient.Core().Endpoints(api.NamespaceAll).Watch(options)
 			},
 		},
-		&v1.Service{},
+		&v1.endpoints{},
 		// resync is not needed
 		0,
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    slm.enqueueService,
-			UpdateFunc: slm.enqueueService,
-			DeleteFunc: slm.enqueueService,
+			AddFunc:    slm.enqueueendpoint,
+			UpdateFunc: slm.enqueueendpoint,
+			DeleteFunc: slm.enqueueendpoint,
 		},
 	)
 }
 
-// obj could be an *api.ReplicationController, or a DeletionFinalStateUnknown marker item.
-func (rm *ServiceLookupManager) enqueuePod(obj interface{}) {
-	key, err := controller.KeyFunc(obj)
-	if err != nil {
-		glog.Errorf("Couldn't get key for object %+v: %v", obj, err)
+// Run begins watching and syncing.
+func (slm *serviceLookupController) Run(workers int, stopCh <-chan struct{}) {
+	defer utilruntime.HandleCrash()
+	fmt.Println("Starting serviceLookupController Manager")
+	go slm.podController.Run(stopCh)
+	go slm.endpointController.Run(stopCh)
+	// wait for the controller to List. This help avoid churns during start up.
+	if !cache.WaitForCacheSync(stopCh, slm.podController.HasSynced, slm.endpointController.HasSynced) {
 		return
 	}
+	for i := 0; i < workers; i++ {
+		go wait.Until(slm.podWorker, time.Second, stopCh)
+		go wait.Until(slm.endpointWorker, time.Second, stopCh)
+	}
 
-	// TODO: Handle overlapping controllers better. Either disallow them at admission time or
-	// deterministically avoid syncing controllers that fight over pods. Currently, we only
-	// ensure that the same controller is synced for a given pod. When we periodically relist
-	// all controllers there will still be some replica instability. One way to handle this is
-	// by querying the store for all controllers that this rc overlaps, as well as all
-	// controllers that overlap this rc, and sorting them.
-	rm.queue.Add(key)
+	<-stopCh
+	fmt.Printf("Shutting down Service Lookup Controller")
+	slm.podsQueue.ShutDown()
+	slm.endpointsQueue.ShutDown()
 }
--- types.go
+++ types.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"sync"
+
+	"k8s.io/client-go/pkg/api/v1"
+)
+
 // Only required to authenticate against GKE clusters
 //_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
@@ -8,4 +14,22 @@ type PodToService struct {
 	PodUID         string `json:"podUID"`
 	ServiceName    string `json:"serviceName,omitempty"`
 	ServiceAddress string `json:"serviceAddress,omitempty"`
+}
+
+type addressToPod struct {
+	core map[string]*v1.Pod
+	lock sync.RWMutex
+}
+
+func (a *addressToPod) Read(address string) (*v1.Pod, bool) {
+	lock.RLock()
+	defer lock.RUnlock()
+	pod, ok := a[address]
+	return pod, ok
+}
+
+func (a *addressToPod) Write(address string, pod *v1.Pod) {
+	lock.Lock()
+	defer lock.Unlock()
+	a[address] = pod
 }
